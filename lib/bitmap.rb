cab22ec07c2f01567c17b739abe4e0905b8197cc
Tr4pSt3R <mightyj@hotmail.co.uk>
2015-01-06 16:06:01 +0000

    Add basic interactive console session

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -3,3 +3,14 @@ VALID_CMDS = ["I", "C", "L", "V", "H", "F", "S", "X"]
 def is_valid_command?( arg )
   VALID_CMDS.include? arg
 end
+
+def main
+  cmd = "I"
+  while( cmd != "X" ) do
+    print "(bitmap) > "
+    a = gets.chomp
+    cmd = a[0]
+  end
+end
+
+main
